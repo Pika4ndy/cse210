c409f67d5717806bc5022444e24efc567760c99c
pika4ndy <aandrianarivo1@byupathway.edu>
2026-07-01 18:02:36 +0300

    finished week1 assignments

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,61 @@
+// Lists & Generics: numbers statistic
+
+/* 
+Program Description:
+    This program will ask a number to the user repeatedly until he inputs 0
+    Then it will display data about it like:
+        - sum of all numbers
+        - the average
+        - the highest number
+        - the lowest positive number
+        - a new sorted list of the numbers
+*/
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<float> userNumberList = new List<float>();
+        float userNumber = 1;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+
+            userNumber = float.Parse(userInput);
+
+            userNumberList.Add(userNumber);
+            
+            
+        } while (userNumber != 0);
+
+        userNumberList.Remove(0);
+
+        userNumberList = userNumberList.DefaultIfEmpty(0).ToList();
+
+        float numbersSum = userNumberList.Sum();
+
+        float average = userNumberList.Average();
+
+        float highestNumber = userNumberList.Max();
+        float lowestPositive = userNumberList.Where(x => x > 0).DefaultIfEmpty(0).Min();
+
+        userNumberList.Sort();
+
+        Console.WriteLine($"The sum of all numbers is: {numbersSum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The highest number is: {highestNumber}");
+        Console.WriteLine($"The lowest positive number is: {lowestPositive}");
+        Console.WriteLine($"The sorted list is:");
+                
+        foreach (int number in userNumberList)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,52 @@
+// Functions: Tell me about my number
+/* 
+    This program will ask the user for his name and favorite number
+    then will display the square of it.
+
+    I think I can push it further
+*/
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+
+        return int.Parse(userInput);
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
