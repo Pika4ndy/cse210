c66b16f78ba1e94a14e1f7d6239e427bc14744d6
pika4ndy <aandrianarivo1@byupathway.edu>
2026-07-30 17:54:29 +0200

    Finished: W5 Inheritance Assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment homework = new Assignment("Joe Doe", "Multiplication");
+
+        Console.WriteLine(homework.GetSummary());
+
+        MathAssignment mathHomework = new MathAssignment("Clara Goldberg", "Integrals", "7.1", "8-10, 15");
+
+        Console.WriteLine();
+        Console.WriteLine(mathHomework.GetSummary());
+        Console.WriteLine(mathHomework.GetHomeworkList());
+
+        WritingAssignment historyHomework = new WritingAssignment("Sonic Hegdehog", "History", "World War II");
+
+        Console.WriteLine();
+        Console.WriteLine(historyHomework.GetSummary());
+        Console.WriteLine(historyHomework.GetWritingInformation());
     }
 }
